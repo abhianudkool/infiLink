1a167f952c259a3a4ea03d5bf31be8029ad76590
Gaurav <gauravb7090@gmail.com>
2014-07-03 17:49:40 +0530

    updated test.js

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
+document.getElementById("button1").addEventListener("click",abc);
+
 function abc(){
+        alert("Tondu bohat dimag khaata hai");
 	$.jStorage.set("a",1);
 	$.jStorage.set("b",2);
 	$.jStorage.set("c",3);
 	$.jStorage.set("d",4);
 
 	value = $.jStorage.get("a");
+
 	alert(value);
-
-
 }
